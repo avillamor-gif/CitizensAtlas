e5805102773c8041858642fc8b15c82c3d0c86c4
Akawar <akawar@gmail.com>
2026-06-14 15:13:21 +0800

    Fix map page layout to prevent footer from appearing - use full screen height with overflow hidden

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -69,12 +69,12 @@ export default function Map() {
   }, [filters])
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-screen overflow-hidden">
       <Header 
         activePage="map"
         onNavigate={handleNavigate}
       />
-      <main className="flex-grow">
+      <main className="flex-grow overflow-hidden">
         <MapPage 
           projects={filteredProjects}
           filters={filters}
